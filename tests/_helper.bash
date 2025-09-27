6292b9b8706a5c600a6eba75237723fcdcd05add
Princeton <info@princetonstrong.online>
2025-09-27 00:24:43 -0400

    bash lib path export #3

--- tests/_helper.bash
+++ tests/_helper.bash
@@ -1,10 +1,8 @@
 # tests/_helper.bash
 
-# Set fallback if CI didn't set it
-export BATS_LIB_PATH=${BATS_LIB_PATH:-"/usr/lib"}
 
 # Load bats libraries
-bats_load_library bats-support
-bats_load_library bats-assert
-bats_load_library bats-file
-bats_load_library bats-detik/detik.bash
+load 'bats-support/load'
+load 'bats-assert/load'
+load 'bats-file/load'
+load 'bats-detik/detik.bash'
